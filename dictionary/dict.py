f6d7605891f66c4c54a79adfdc253e66c2028dce
dineshmakkar079 <dineshmakkar079@gmail.com>
2017-06-11 01:31:02 +0530

    Adds word to dictionary if new word is seacrhed & more randomly picks words

--- dictionary/dict.py
+++ dictionary/dict.py
@@ -1,11 +1,16 @@
 import urllib.request
 import sys
 import random
+import os
+import io
 import wordlist
+
+word=""
 if(len(sys.argv) == 1):
-	word = wordlist.wordlist[random.randrange(500, 9995,100)]	
+	word = wordlist.wordlist[random.randrange(500, len(wordlist.wordlist), 10)]	
 else:
 	word = sys.argv[1];
+
 print(word);
 url = "http://api.pearson.com/v2/dictionaries/entries?headword=" + word
 import json
@@ -17,12 +22,20 @@ with urllib.request.urlopen(url) as resp:
 		try :
 			for sense in el['senses']:
 				defArr = sense['definition']
-				if(len(defArr) == 1):
-					for defi in sense['definition']:
+				if count < 3 :
+					if len(defArr) == 1 :
+						print('>  ' + defArr[0])
 						count = count + 1
-						print('>  ' + defi)
-				else:
-					count =  count + 1
-					print('>  ' + defi)
+					else:
+						print('>  ' + defArr)
+						count = count + 1
 		except:
 			pass
+
+if not count == 0:
+	if not word in wordlist.wordlist:
+		with open(os.path.dirname(os.path.realpath(__file__))+ os.sep + 'wordlist.py','r+') as wri : 
+			wri.seek(0,2);
+			wri.seek(wri.tell()-1,0);
+			wri.write(',"' + word + '"]')
+		print("* New word added to dictionary.")
